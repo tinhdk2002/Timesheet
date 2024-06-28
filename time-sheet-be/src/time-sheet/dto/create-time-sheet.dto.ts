1b1de5b0008693d313c8a179fa43f7a5109af9e6
Dương Văn Tình <130557041+tinhdk2002@users.noreply.github.com>
2024-06-28 23:06:23 +0700

    Update create-time-sheet.dto.ts

--- time-sheet-be/src/time-sheet/dto/create-time-sheet.dto.ts
+++ time-sheet-be/src/time-sheet/dto/create-time-sheet.dto.ts
@@ -7,30 +7,37 @@ import { User } from "src/user/entities/user.entity"
 
 export class CreateTimeSheetDto {
     @IsString()
+    @IsNotEmpty()
     @ApiProperty()
     note: string
 
     @IsString()
+    @IsNotEmpty()
     @ApiProperty()
     time: number
 
     @IsString()
+    @IsNotEmpty()
     @ApiProperty()
     type: string
 
+    @IsNotEmpty()
     @IsNotEmpty()
     @ApiProperty()
     projects: Project
 
     @IsNotEmpty()
+    @IsNotEmpty()
     @ApiProperty()
     tasks: Task
 
     @IsString()
+    @IsNotEmpty()
     @ApiProperty()
     date: Date
 
     @IsEnum(StatusTS)
+    @IsNotEmpty()
     @ApiProperty()
     status?: StatusTS
 
