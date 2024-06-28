3d4fa0397f3c9627ed700ab75999af9a30dfb645
Dương Văn Tình <130557041+tinhdk2002@users.noreply.github.com>
2024-06-28 23:02:54 +0700

    Update logingg.dto.ts

--- time-sheet-be/src/auth/dto/logingg.dto.ts
+++ time-sheet-be/src/auth/dto/logingg.dto.ts
@@ -1,7 +1,9 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty } from "class-validator";
 
 export class LoginGoogleDto {
   @ApiProperty()
+  @IsNotEmpty()
   email: string;
 
 }
