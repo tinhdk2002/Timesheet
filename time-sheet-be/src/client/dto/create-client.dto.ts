d32dea7a7fa73f4acdacf52b028b3d85e7d60e58
Dương Văn Tình <130557041+tinhdk2002@users.noreply.github.com>
2024-06-28 23:03:34 +0700

    Update create-client.dto.ts

--- time-sheet-be/src/client/dto/create-client.dto.ts
+++ time-sheet-be/src/client/dto/create-client.dto.ts
@@ -1,16 +1,19 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsString } from "class-validator";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateClientDto {
     @ApiProperty()
+    @IsNotEmpty()
     @IsString()
     name:string;
 
     @ApiProperty()
+    @IsNotEmpty()
     @IsString()
     code: string;
 
     @ApiProperty()
+    @IsNotEmpty()
     @IsString()
     address?: string;
 }
